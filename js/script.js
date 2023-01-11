de5546647b9a616fbea5ca989667c4701f7c3d44
Daniel Kowalski <77574206+k0wal45@users.noreply.github.com>
2023-01-11 16:43:26 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function change() {
+    document.getElementById('btn-change').innerHTML = '+48 604 47 66 47<br>+48 608 403 675'
+}
+
+function fontbold() {
+
+    document.getElementById("body").style.fontWeight = 'bold'
+}
+
+function fontnorm() {
+
+    document.getElementById("body").style.fontWeight = '300'
+}
+
+
+function replace2() {
+    document.getElementById('galery-zwyzka').innerHTML = '<img src="../imagine/zdjecia/klimy/klima1.jpg" alt=""> <img src="../imagine/zdjecia/klimy/klima2.jpg" alt=""> <img src="../imagine/zdjecia/klimy/klima9.jpg" alt=""> <img src="../imagine/zdjecia/klimy/klima4.jpg" alt=""> <img src="../imagine/zdjecia/klimy/klima5.jpg" alt=""> <img src="../imagine/zdjecia/klimy/klima6.jpg" alt=""> <img src="../imagine/zdjecia/klimy/klima7.jpg" alt=""> <img src="../imagine/zdjecia/klimy/klima8.jpg" alt=""> <img src="../imagine/zdjecia/klimy/klima3.jpg" alt=""> '
+}
+
+function replace1() {
+    document.getElementById('galery-zwyzka').innerHTML = '<img src="../imagine/zdjecia/zwyzka/zwyzka1.jpg" alt=""> <img src="../imagine/zdjecia/zwyzka/zwyzka2.jpg" alt=""> <img src="../imagine/zdjecia/zwyzka/zwyzka9.jpg" alt=""> <img src="../imagine/zdjecia/zwyzka/zwyzka4.jpg" alt=""> <img src="../imagine/zdjecia/zwyzka/zwyzka5.jpg" alt=""> <img src="../imagine/zdjecia/zwyzka/zwyzka6.jpg" alt=""> <img src="../imagine/zdjecia/zwyzka/zwyzka7.jpg" alt=""> <img src="../imagine/zdjecia/zwyzka/zwyzka8.jpg" alt=""> <img src="../imagine/zdjecia/zwyzka/zwyzka3.jpg" alt=""> '
+}
